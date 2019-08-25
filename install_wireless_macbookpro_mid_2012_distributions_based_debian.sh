c2596dc20f6f0775c77b0c5ec3a4611c69729ee3
Kaisen <kevinchevreuillinux@gmail.com>
2019-08-25 22:30:53 +0200

    Update install_wireless_macbookpro_mid_2012_distributions_based_debian.sh

--- install_wireless_macbookpro_mid_2012_distributions_based_debian.sh
+++ install_wireless_macbookpro_mid_2012_distributions_based_debian.sh
@@ -3,7 +3,7 @@
 echo 'This script activates a wireless card on a MacBookPro 9.2 (Mid-2012) after Ubuntu install and Debian install'
 
 #Add repository for download and install firmware-b43
-echo 'deb http://deb.debian.org/debian/ stretch main contrib non-free' | sudo tee -a /etc/apt/sources.list
+echo 'deb http://deb.debian.org/debian/ stretch main contrib non-free' | sudo tee -a /etc/apt/sources.list.d/wifimac.list
 #Update repositories for applicates modifications in /etc/apt/sources.list
 sudo apt-get update
 #Install firmware-b43-(wireless driver)
