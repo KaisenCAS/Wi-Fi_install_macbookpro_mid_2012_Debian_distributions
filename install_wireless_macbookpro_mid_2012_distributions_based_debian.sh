f69ddf392f77da3d3d7a623f4c802d10e800c8d0
Kaisen <kaisen@localhost.localdomain>
2019-12-24 01:20:38 +0100

    Add checking root execution

--- install_wireless_macbookpro_mid_2012_distributions_based_debian.sh
+++ install_wireless_macbookpro_mid_2012_distributions_based_debian.sh
@@ -1,12 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+
+#Check root execution verification
+if [[ "$EUID" -ne 0 ]]; then
+	echo -e "Run this script as root"
+	exit 1
+fi
 
 echo 'This script activates a wireless card on a MacBookPro 9.2 (Mid-2012) after Ubuntu install and Debian install'
 
 #Add repository for download and install firmware-b43
-echo 'deb http://deb.debian.org/debian/ stretch main contrib non-free' | sudo tee -a /etc/apt/sources.list.d/wifimac.list
+echo 'deb http://deb.debian.org/debian/ stretch main contrib non-free' | tee -a /etc/apt/sources.list.d/wifimac.list
 #Update repositories for applicates modifications in /etc/apt/sources.list
-sudo apt-get update
+apt-get update
 #Install firmware-b43-(wireless driver)
-sudo apt-get install firmware-b43-installer -y
+apt-get install firmware-b43-installer -y
 #Reboot for activate wireless card
-sudo reboot
+reboot
